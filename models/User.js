c5af983608dd1979ee415555ad2fc6f2747ad1ef
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-11-23 02:20:45 +0500

    code review in user model

--- models/User.js
+++ models/User.js
@@ -1,23 +1,33 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  name: { type: String, required: [true, "name is required"] },
-  lastname: { type: String, required: [true, "lastname is required"] },
-  email: {
-    type: String,
-    // validate: {
-    //   validator: function (v) {
-    //     return /\S+@\S+\.\S+/.test(v);
-    //   },
-    message: "Email is not valid",
-  },
-  phone_number: { type: Number, required: [true, "phone_number is required"] },
-  password: {
-    type: String,
-    required: true,
-    minlength: [6, "password must be 6 chracter"],
-  },
+	firstname: {
+		type: String,
+		required: true,
+	},
+	lastname: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+	},
+	phone_number: {
+		type: Number | String,
+		required: true,
+	},
+	address: {
+		type: String,
+		default: '',
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: [6, 'password must be 6 character'],
+	},
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model('User', userSchema);

bfa4fcc5622a3aa87dc8b3f0403e4c6c950ba55a
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-11-23 01:27:25 +0500

    changed port

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const mongodb = require("mongodb");
+const express = require('express');
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 const app = express();
-const bodyParser = require("body-parser");
-const User = require("./models/user");
+const bodyParser = require('body-parser');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const userRouter = require("./api/routes/user");
+const userRouter = require('./api/routes/user');
 
-app.use("/user", userRouter);
+app.use('/user', userRouter);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 mongoose
-  .connect("mongodb://localhost:27017/project", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`http://localhost:${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+	.connect('mongodb://localhost:27017/project', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(port, () => {
+			console.log(`http://localhost:${port}`);
+		});
+	})
+	.catch(err => {
+		console.log(err);
+	});

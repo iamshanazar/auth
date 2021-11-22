489b46c4a492304b1353b16bedbf481e5021fa2e
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-11-23 02:21:16 +0500

    deleted api folder within files

--- api/routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../../Controller/user");
-
-router.post("/signup", userController.user_post_signup);
-
-module.exports = router;

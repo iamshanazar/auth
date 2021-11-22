4987c2d458495f4090266cef860ffb009dda33c9
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-11-23 01:51:36 +0500

    require dotenv in app.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+const DB_NAME = process.env.DB_NAME || 'ecommerce';
 
 mongoose
-	.connect('mongodb://localhost:27017/project', {
+	.connect(`mongodb://localhost:27017/${DB_NAME}`, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
 	.then(() => {
-		app.listen(port, () => {
-			console.log(`http://localhost:${port}`);
+		app.listen(PORT, () => {
+			console.log(`http://localhost:${PORT}`);
 		});
 	})
 	.catch(err => {
